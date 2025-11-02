4da49a84eb852f999fb88b964f627e9769ffda3d
SurajPrajapati8424 <surajprajapati8286@gmail.com>
2025-11-02 10:21:03 +0530

    Remove config.js from git tracking (now ignored)

--- web-interface/config.js
+++
@@ -1,8 +0,0 @@
-const CONFIG = {
-    apiEndpoint: 'https://5w25yx5fig.execute-api.us-east-1.amazonaws.com/dev',
-    region: 'us-east-1',
-    userPoolId: 'us-east-1_NMhOEEsBV',
-    userPoolClientId: '1e9d6o6g647tt981b5rv0nssok',
-    identityPoolId: 'us-east-1:542a97f9-125b-4f25-be2e-a5b33a0fa6d2',
-    environment: 'dev'
-};
